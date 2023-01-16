4ed93c6f07a5106846f1c7da66312ac162d6faf6
riyasklb <riyasklb89@gmail.com>
2023-01-16 12:44:34 +0530

    first commit

---
+++ lib/dart_application_22.dart
@@ -0,0 +1,25 @@
+//import 'package:dart_application_22/dart_application_22.dart'
+//    as dart_application_22;
+
+void main(List<String> arguments) {
+  int i;
+  var data = 0;
+  var sum = 0;
+  for (i = 1; i <= 10; i += 2) {
+    data = data + i;
+  }
+  for (i = 2; i <= 10; i += 2) {
+    sum = sum + i;
+  }
+  print('sum of odd no.$data ');
+  print('sum of even no.$sum');
+}
+
+//data = data + i;
+  //  print(data);
+  
+
+//   var set = {1, 2, 3, 4, 5};
+//   for (set in data);
+//   print(data);
+// }
